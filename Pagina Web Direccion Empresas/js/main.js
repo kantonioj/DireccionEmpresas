942af7ccc868afac50db0d1cd1dca7a6ccab9cad
kantonioj <32857947+kantonioj@users.noreply.github.com>
2018-02-19 10:11:14 -0600

    Add files via upload

--- Pagina Web Direccion Empresas/js/main.js
+++ Pagina Web Direccion Empresas/js/main.js
@@ -154,6 +154,14 @@ $(document).ready(function(){
       $(".table-total").append(th);
     }
 
+    for (var i = 1; i <= global_inputs; i++) {
+      $("#H" + i).attr("title", "" + $("#S" + i).val());
+    }
+
+    for (var i = 1; i <= 6; i++) {
+      $("#Ti" + i).attr("title", "" + $("#input-C" + i).val());
+    }
+
 
     return false;
   });
@@ -168,9 +176,45 @@ $(document).ready(function(){
       Sol +=  parseInt($("#U" + i).children().val());
       Sol +=  parseInt($("#R" + i).children().val());
       Sol +=  parseInt($("#I" + i).children().val());
-      console.log(Sol);
       $("#T" + i).text(Sol.toString());
     }
+    var arr = [];
+    var max = parseInt($("#T1").text());
+    for (var i = 2; i <= global_inputs; i++) {
+      if(max < parseInt($("#T" + i).text())){
+        max = parseInt($("#T" + i).text());
+      }
+      console.log("MAX ES:" + max);
+    }
+
+    for (var i = 1; i <= global_inputs; i++) {
+      if(max == parseInt($("#T" + i).text())){
+        arr.push($("#S" + i).val());
+      }
+    }
+    console.log(arr.length);
+
+    if (arr.length < 1) {
+      $("#final-solution-text").text('La solución para tu problema o caso es: "' + arr[0] + '"');
+    } else {
+      $("#final-solution-text").text('La solución para tu problema o caso es: ');
+      for (var i = 0; i < arr.length; i++) {
+        $("#final-solution-text").append('"' + arr[i] +'"');
+        if (i < arr.length - 2) { $("#final-solution-text").append(', '); }
+        else if (i < arr.length - 1) { $("#final-solution-text").append(' y '); }
+
+      }
+    }
+
+    if(max <= 20){
+      $(note).text("Nota: La solución es funcional pero no parece ser óptima, le recomendamos que revise su soluciones, los valores que le asignó a la matriz o vuelva a replantear el problema o caso.");
+      $(note).css({"background-color" : "#ffffcc", "padding" : "5px 15px", "color" : "black"});
+      if (max <= 10) {
+        $(note).text("Nota: La solución no es ótima ni funcional. Porfavor vuelva a plantear todo de nuevo para poder tener una solución a su problema");
+        $(note).css({"background-color" : "firebrick", "padding" : "5px 15px", "color" : "white"});
+      }
+    } else { $(note).text(""); $(note).css({"background-color" : "#b5b7b9"}); }
+
     return false;
   });
 
